a5b6df6ba167698c378c68c5940e5e6f9ddb1155
FernandoSzkvarak <fernandodesengrini11@gmail.com>
2024-09-05 15:46:58 -0300

    Implementação do CRUD ATV

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, FastAPI
 
-from app.api.routes import items, login, users, utils
+from app.api.routes import items, login, users, utils, inventory 
+
+app = FastAPI()
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
+api_router.include_router(inventory.inventory_router, prefix="/inventory", tags=["inventory"])
+
+app.include_router(api_router)
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,114 +1,68 @@
 import uuid
-
 from pydantic import EmailStr
 from sqlmodel import Field, Relationship, SQLModel
 
-
-# Shared properties
 class UserBase(SQLModel):
     email: EmailStr = Field(unique=True, index=True, max_length=255)
     is_active: bool = True
     is_superuser: bool = False
     full_name: str | None = Field(default=None, max_length=255)
 
-
-# Properties to receive via API on creation
 class UserCreate(UserBase):
     password: str = Field(min_length=8, max_length=40)
-
 
 class UserRegister(SQLModel):
     email: EmailStr = Field(max_length=255)
     password: str = Field(min_length=8, max_length=40)
     full_name: str | None = Field(default=None, max_length=255)
 
-
-# Properties to receive via API on update, all are optional
 class UserUpdate(UserBase):
-    email: EmailStr | None = Field(default=None, max_length=255)  # type: ignore
+    email: EmailStr | None = Field(default=None, max_length=255)
     password: str | None = Field(default=None, min_length=8, max_length=40)
-
 
 class UserUpdateMe(SQLModel):
     full_name: str | None = Field(default=None, max_length=255)
     email: EmailStr | None = Field(default=None, max_length=255)
 
-
 class UpdatePassword(SQLModel):
     current_password: str = Field(min_length=8, max_length=40)
     new_password: str = Field(min_length=8, max_length=40)
 
-
-# Database model, database table inferred from class name
 class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     hashed_password: str
-    items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
+    inventory_items: list["InventoryItem"] = Relationship(back_populates="owner", cascade="all, delete")
 
-
-# Properties to return via API, id is always required
 class UserPublic(UserBase):
     id: uuid.UUID
-
 
 class UsersPublic(SQLModel):
     data: list[UserPublic]
     count: int
 
-
-# Shared properties
-class ItemBase(SQLModel):
-    title: str = Field(min_length=1, max_length=255)
+class InventoryBase(SQLModel):
+    name: str = Field(max_length=255, nullable=False)
     description: str | None = Field(default=None, max_length=255)
+    price: float = Field(gt=0, nullable=False)
+    stock: int = Field(ge=0, default=0, nullable=False)
 
-
-# Properties to receive on item creation
-class ItemCreate(ItemBase):
+class InventoryCreate(InventoryBase):
     pass
 
+class InventoryUpdate(InventoryBase):
+    name: str | None = Field(default=None, max_length=255)
+    price: float | None = Field(default=None, gt=0)
+    stock: int | None = Field(default=None, ge=0)
 
-# Properties to receive on item update
-class ItemUpdate(ItemBase):
-    title: str | None = Field(default=None, min_length=1, max_length=255)  # type: ignore
-
-
-# Database model, database table inferred from class name
-class Item(ItemBase, table=True):
+class InventoryItem(InventoryBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    title: str = Field(max_length=255)
-    owner_id: uuid.UUID = Field(
-        foreign_key="user.id", nullable=False, ondelete="CASCADE"
-    )
-    owner: User | None = Relationship(back_populates="items")
+    owner_id: uuid.UUID = Field(foreign_key="user.id", nullable=False)
+    owner: User | None = Relationship(back_populates="inventory_items")
 
-
-# Properties to return via API, id is always required
-class ItemPublic(ItemBase):
+class InventoryPublic(InventoryBase):
     id: uuid.UUID
     owner_id: uuid.UUID
 
-
-class ItemsPublic(SQLModel):
-    data: list[ItemPublic]
+class InventoriesPublic(SQLModel):
+    data: list[InventoryPublic]
     count: int
-
-
-# Generic message
-class Message(SQLModel):
-    message: str
-
-
-# JSON payload containing access token
-class Token(SQLModel):
-    access_token: str
-    token_type: str = "bearer"
-
-
-# Contents of JWT token
-class TokenPayload(SQLModel):
-    sub: str | None = None
-
-
-class NewPassword(SQLModel):
-    token: str
-    new_password: str = Field(min_length=8, max_length=40)
